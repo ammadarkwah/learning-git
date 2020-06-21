df11aa781e2582082228b50b55b4e74922d83a2c
unknown <ammadarkwah>
2020-06-20 20:18:19 -0400

    i made cchanges to the README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learning-git
+## Amma 
+Learning Github is cool
 This is a repo for learning how to use github
--- hello.java
+++ hello.java
@@ -1,1 +1,7 @@
 //Hello java
+class hello{
+
+     public static void main( String[] argv) {
+        System.out.println("Ohai IUP");
+    }
+}
