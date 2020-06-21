03400141165c10e40dc05bf802c174bbd809611b
Botwe Asamoah Emmanuel <noelnuel44@gmail.com>
2020-06-21 01:17:56 +0000

    added a new line

--- hello.java
+++ hello.java
@@ -6,6 +6,7 @@ class hello {
         System.out.println("Hello, I am Amma,");
         System.out.println("This is my collaborator botchway44");
         System.out.println("We just made a new Branch called Amma-branch");
-        System.out.println("Come check it out");
+        System.out.println("Come check it out.");
+        System.out.println("This change is made by Emma.");
     }
 }
