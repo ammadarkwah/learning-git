b60aa9fa67d4086de2159a6b4f904acef9620021
Botwe Asamoah Emmanuel <noelnuel44@gmail.com>
2020-06-21 00:32:59 +0000

    made changes to amma files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # learning-git
 ## Amma 
+## Botchway44
 Learning Github is cool
 This is a repo for learning how to use github
--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 //Hello java
-class hello{
+class hello {
 
-     public static void main( String[] argv) {
+    public static void main(String[] argv) {
         System.out.println("Ohai IUP");
+        System.out.println("Hello, I am Amma,");
+        System.out.println("This is my collaborator botchway44");
     }
 }
